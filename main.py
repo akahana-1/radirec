9980b30c5f45bd113205ecc9a447e4dca4b8f977
akahana-1 <h163341@edu.tut.ac.jp>
2017-01-11 00:14:34 +0900

    デバッグをやりやすくした

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--record_dir", type = str, default = "./recorded")
     parser.add_argument("--config_dir", type = str, default = ".")
+    parser.add_argument("--debug", action = 'store_true')
 
     return vars(parser.parse_args())
 
 def main():
     args = parse()
+
+    debug = args["debug"]
+    del(args["debug"])
+
     ctrl = Controller(**args)
-    ctrl.check(debug = True)
+    ctrl.check(debug)
 
 if __name__ == '__main__':
     main()

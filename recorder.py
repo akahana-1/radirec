3313d5aab0b2d8ac79bfbcecbc9724898b87da0b
akahana-1 <h163341@edu.tut.ac.jp>
2017-03-26 13:00:31 +0900

    並列実行に対応したが録画の成功失敗が判定できない

--- recorder.py
+++ recorder.py
@@ -1,20 +1,29 @@
 import subprocess
 import shlex
-import time
 
 
 def record_agqr(p):
-    cmd = "/usr/bin/rtmpdump"
+    cmd = "/usr/bin/rtmpdump -r {url} --live -B {length} -o {file}"
     url = "rtmp://fms-base1.mitene.ad.jp/agqr/aandg22"
     cmdopt = {
-        "-r": url,
-        "--live": None,
-        "-B": p["length"],
-        "-o": "hoge.flv"
+        "length": p["length"],
+        "file": "hoge.flv"
         }
-    cmds = [cmd, ] + [_ for c in cmdopt.items() for _ in c if _]
+    cmds = shlex.split(cmd.format(url=url, **cmdopt))
+    p = subprocess.Popen(cmds, stderr=subprocess.DEVNULL)
+
+
+def record_chofu(p):
+    cmd = "/usr/bin/ffmpeg -i {url} -t {length} {file}"
+    url = "mmsh://hdv3.nkansai.tv/chofu?MSWMExt=.asf"
+    cmdopt = {
+            "length": p["length"],
+            "file": "fuga.mp3"
+            }
+    cmds = shlex.split(cmd.format(url=url, **cmdopt))
     p = subprocess.Popen(cmds, stderr=subprocess.DEVNULL)
 
 if __name__ == '__main__':
-    record_agqr({"length": "100"})
+    record_agqr({"length": "30"})
+    record_chofu({"length": "30"})
     print("complete!")

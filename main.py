09f21ec48635eb66ee24bfefbf94f68ba8fcf14d
akahana-1 <h163341@edu.tut.ac.jp>
2017-04-15 18:17:45 +0900

    色々と修正をした

--- main.py
+++ main.py
@@ -4,13 +4,14 @@
 import os.path
 import argparse
 import asyncio
+import json
 from string import Template
 from datetime import datetime, timedelta
 # from logging import getLogger
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
-from utils import Controller, Recorder
+# from utils import Controller, Recorder
 
 SCHEDULE = "schedule.json"
 CONFIG = "config.json"
@@ -21,10 +22,19 @@ def check_requirement(config):
     tile_format, record_dir, channelsがそれぞれ定義されているか確認
     定義されていなければ適当に例外飛ばす
     """
+    pass
+
+
+def load_config(config_file):
+    with open(config_file, "r") as f:
+        config = json.load(f)
+    return config
 
 
 def assemble_record_command(config, program):
     ch = config["channels"].get(program["channel"], None)
+    params = program
+    params["length"] *= 60
 
     # 録画すべきチャンネルの設定が存在していないときは何もしない
     if ch is None:
@@ -41,54 +51,75 @@ def assemble_record_command(config, program):
         """
         return
 
-    opt = Template(opt).safe_substitute(ch).safe_substitute(program)
+    params.update(ch)
+
+    opt = Template(opt).safe_substitute(params)
     return " ".join((cmd, opt))
 
 
-def assemble_title(config, program, start_time):
+def assemble_title(config, program, date):
     title = Template(config.get["title_format"])
-    """
-    ここに置き換え処理が入る
-    """
+    title.safe_substitute(program)
+    title.safe_substitute(date=date.strftime("%Y%m%d"))
     return title
 
 
-def generate(programs, interval=60):
+def record(config, program, now):
+    cmd = assemble_record_command(config, program)
+    if not cmd:
+        """
+        なんかエラー出力
+        """
+    title = assemble_title(config, program, now)
+    pass
+
+
+def is_matched_program(programs, interval=60):
     now = datetime.now()
+    now = datetime(now.year, 4, 1, 18, 29, 30)
     wday = now.weekday()
 
     for program in programs:
-        h, m = map(int, program["time"].split(":"))
-        is_next_day = int(h == 0 and m == 0)
+        h, m = map(int, program["start"].split(":"))
+        is_next_day = h == 0 and m == 0
 
         start_wday = ("月火水木金土日".find(program["wday"])) % 7
-        start_time = datetime(now.year, now.month, now.day, h, m) + timedelta(days=is_next_day)
+        start_time = datetime(now.year, now.month, now.day, h, m)
+        if is_next_day:
+            start_time += timedelta(days=1)
 
-        is_appropriate_time = abs(start_time - now) < timedelta(seconds=interval)
+        appropriate_interval = timedelta(seconds=interval)
+
+        is_appropriate_time = abs(start_time - now) < appropriate_interval
         is_appropriate_wday = (wday + is_next_day) % 7 == start_wday
 
         if is_appropriate_time and is_appropriate_wday:
-            pass
-
+            yield (start_time, program)
 
 
 def parse():
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_file", type=str, default=CONFIG)
     parser.add_argument("--schedule_file", type=str, default=SCHEDULE)
-    parser.add_argument("--debug", action='store_true')
 
     return vars(parser.parse_args())
 
 
 def main():
     args = parse()
+    config = load_config(args["config_file"])
+    with open(args["schedule_file"], "r") as f:
+        programs = json.load(f)
+    for a in is_matched_program(programs):
+        print(a)
+    # scheduler = AsyncIOScheduler()
+    # scheduler.start()
 
-    scheduler = AsyncIOScheduler()
+    # try:
+    #     asyncio.get_event_loop().run_forever()
+    # except:
+    #     pass
 
-    scheduler.start()
 
-    try:
-        asyncio.get_event_loop().run_forever()
-    except:
-        pass
+if __name__ == '__main__':
+    main()
